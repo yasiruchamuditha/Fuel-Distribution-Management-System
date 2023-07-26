99dbf76a7094507cd265f1945c7e23d20c2e6e34
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2023-07-26 22:02:01 +0530

    26,07,2023 10,01 P.M.

--- User_SignUp.php
+++ User_SignUp.php
@@ -102,7 +102,7 @@ if(isset($_POST["btnSubmit"]))
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'fuelupgroup@gmail.com';                     //SMTP username
-        $mail->Password   = 'tiykxmnknuryvcwt';                               //SMTP password
+        $mail->Password   = 'yxejuwcqpkztsmln';                               //SMTP password
         $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
@@ -156,7 +156,7 @@ if(isset($_POST["btnSubmit"]))
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'fuelupgroup@gmail.com';                     //SMTP username
-        $mail->Password   = 'tiykxmnknuryvcwt';                               //SMTP password
+        $mail->Password   = 'yxejuwcqpkztsmln';                               //SMTP password
         $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
